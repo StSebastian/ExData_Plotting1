cbf2a12008719d98c9346dc61308473f0bce3e21
StSebastian <sebastian.st@mytum.de>
2014-07-12 13:00:49 +0200

    Version3

--- plot2.r
+++ plot2.r
@@ -20,11 +20,17 @@
     dataSample$Date<-as.Date(strptime(dataSample$Date,"%d/%m/%Y"))
     dataSample$Global_active_power<-as.numeric(dataSample$Global_active_power)
     dataSample$Time2<-strptime(paste(dataSample$Date,dataSample$Time),"%Y-%m-%d %H:%M:%S")
+    dataSample$index<-1:nrow(dataSample)
 
 # creating the plot in R 
     par(mfrow=c(1,1))
-    with(dataSample,plot(Time2,Global_active_power,main="",type = "l",ylab="Global Active Power (kilowatts)",xlab=""))
+    with(dataSample,plot(index,Global_active_power,main="",type = "l",ylab="Global Active Power (kilowatts)",xlab="",xaxt='n'))
 
+# adjusting X-axis values (necessary since I'm not using an english R-version with other weekday names         
+    days <- dataSample[,"Time"]=="00:00:00"
+	days[length(days)]<-T
+	axis(1, at=c(dataSample[days,"index"]), labels=c("Thu","Fri","Sat"))    
+    
 # saving plot 2
     dev.copy(png, file = "plot2.png",width=480,height=480) 
     dev.off()
--- plot3.r
+++ plot3.r
@@ -20,18 +20,26 @@
     dataSample$Date<-as.Date(strptime(dataSample$Date,"%d/%m/%Y"))
     dataSample$Global_active_power<-as.numeric(dataSample$Global_active_power)
     dataSample$Time2<-strptime(paste(dataSample$Date,dataSample$Time),"%Y-%m-%d %H:%M:%S")
+    dataSample$index<-1:nrow(dataSample)
     dataSample$Sub_metering_1<-as.numeric(dataSample$Sub_metering_1)
     dataSample$Sub_metering_2<-as.numeric(dataSample$Sub_metering_2)
     dataSample$Sub_metering_3<-as.numeric(dataSample$Sub_metering_3)
 
+    
 # creating the plot in R 
     par(mfrow=c(1,1))
-    with(dataSample,plot(Time2,Sub_metering_1,main="",type = "l",ylab="Energy sub metering",xlab=""))
-    with(dataSample,points(Time2,Sub_metering_2,type = "l",col="red"))
-    with(dataSample,points(Time2,Sub_metering_3,type = "l",col="blue"))
+    with(dataSample,plot(index,Sub_metering_1,main="",type = "l",ylab="Energy sub metering",xlab="",xaxt='n'))
+    with(dataSample,points(index,Sub_metering_2,type = "l",col="red"))
+    with(dataSample,points(index,Sub_metering_3,type = "l",col="blue"))
     legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
         lty=c(1,1),col = c("black","blue", "red"), cex=0.8)
 
+# adjusting X-axis values (necessary since I'm not using an english R-version with other weekday names         
+    days <- dataSample[,"Time"]=="00:00:00"
+	days[length(days)]<-T
+	axis(1, at=c(dataSample[days,"index"]), labels=c("Thu","Fri","Sat"))
+    
+        
 # saving plot 3
     dev.copy(png, file = "plot3.png",width=480,height=480) 
     dev.off()
